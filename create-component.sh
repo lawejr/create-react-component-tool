3055f35c4ed731a595997d18d2d1588e63fdb6ac
lawejr <kalinin.aa@pik.ru>
2021-03-18 15:55:12 +0500

    Генерация шаблона тестов вынесена в отдельную директорию

--- create-component.sh
+++ create-component.sh
@@ -2,14 +2,16 @@
 TEMPLATE_NAME="templateName"
 TEMPLATE_DIR="`dirname "$0"`/templates/component"
 TEMPLATE_PATH="$TEMPLATE_DIR/$TEMPLATE_NAME"
+TESTS_DIR="__tests__"
 NAME=$1
 
 mkdir $NAME
+mkdir "$NAME/$TESTS_DIR"
 
 cp "$TEMPLATE_DIR/index.ts" "$PWD/$NAME/index.ts"
 cp "$TEMPLATE_DIR/types.ts" "$PWD/$NAME/types.ts"
 cp "$TEMPLATE_PATH.tsx" "$PWD/$NAME/$NAME.tsx"
-cp "$TEMPLATE_PATH.test.tsx" "$PWD/$NAME/$NAME.test.tsx"
+cp "$TEMPLATE_PATH.test.tsx" "$PWD/$NAME/$TESTS_DIR/$NAME.test.tsx"
 cp "$TEMPLATE_PATH.stories.tsx" "$PWD/$NAME/$NAME.stories.tsx"
 cp "$TEMPLATE_PATH.module.css" "$PWD/$NAME/$NAME.module.css"
 
