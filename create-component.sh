5be661381f5d24aca57c0d2f98c85cb5dd1e6df7
lawejr <kalinin.aa@pik.ru>
2021-03-18 12:32:18 +0500

    fix: Добавлена генерация для типов и интерфейсов

--- create-component.sh
+++ create-component.sh
@@ -7,7 +7,7 @@ NAME=$1
 mkdir $NAME
 
 cp "$TEMPLATE_DIR/index.ts" "$PWD/$NAME/index.ts"
-cp "$TEMPLATE_DIR.ts" "$PWD/$NAME/types.ts"
+cp "$TEMPLATE_DIR/types.ts" "$PWD/$NAME/types.ts"
 cp "$TEMPLATE_PATH.tsx" "$PWD/$NAME/$NAME.tsx"
 cp "$TEMPLATE_PATH.test.tsx" "$PWD/$NAME/$NAME.test.tsx"
 cp "$TEMPLATE_PATH.stories.tsx" "$PWD/$NAME/$NAME.stories.tsx"
